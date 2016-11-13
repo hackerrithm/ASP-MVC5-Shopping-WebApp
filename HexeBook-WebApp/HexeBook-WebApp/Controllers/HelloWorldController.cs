edec6f29e858deecbbe3241174b804dacb5de113
gallo <gallo@DESKTOP-G5407EH>
2016-11-12 22:31:59 -0500

    Tested implementation of a simple hello world controller

---
+++ HexeBook-WebApp/HexeBook-WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HexeBook_WebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "Thi is my <b>default</b> action 2";
+            //return View();
+        }
+
+        public string Welcome()
+        {
+            return "This is my welcome method";
+        }
+    }
+}
